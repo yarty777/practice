1d50166cdb47211891b7e85bbe295444c2b84708
yarty777 <yaroslav0908l@gmail.com>
2026-01-21 20:15:38 +0200

    Minor student changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,34 @@
 import './App.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import {StudentDashboard,StudentLabs} from './Student.js'
 
 function App() {
   return (
     <>
     <BrowserRouter>
+    <Link to="/login" style={{ textDecoration: 'none' }}>
+        <button>Перейти на логін</button>
+    </Link>
+    <Link to="/" style={{ textDecoration: 'none' }}>
+        <button>Перейти додому</button>
+    </Link>
+    <Link to="/student/dashboard" style={{ textDecoration: 'none' }}>
+        <button>Перейти до профілю студента</button>
+    </Link>
+    <Link to="/student/labs" style={{ textDecoration: 'none' }}>
+        <button>Список лаб</button>
+    </Link>
      <Routes>
           <Route path="/" element={<Home />} />
           {/*Головна*/}
           <Route path="/login" element={<Login />} />
           {/*Реєстрація*/}
 
-          {/*<Route path="/student/dashboard" element={<StudentDashboard />} />
-          {профіль студента (ПІБ група статистика)}
+          <Route path="/student/dashboard" element={<StudentDashboard />} />
+          {/*профіль студента (ПІБ група статистика)*/}
           <Route path="/student/labs" element={<StudentLabs />} />
-          {список лаб}
-          
+          {/*список лаб}
+          {/*
           <Route path="/teacher/dashboard" element={<TeacherDashboard />} />
           {статистика по групам і профіль вчителя}
           <Route path="/teacher/students" element={<TeacherStudents />} />
@@ -35,7 +48,7 @@ function Home(){
 
   return (
     <>
-    
+
     </>
   )
 }
